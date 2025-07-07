90d7fb94e3b149a43020619151493d58b71c50fa
Rohit Priyadarshi <rohitp2203@gmail.com>
2025-07-08 00:46:51 +0530

    tailwind setup done

---
+++ scm2.0/tailwind.cnfig.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // adjust based on your framework (React, Vue, etc.)
+    "./src/main/resources/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
